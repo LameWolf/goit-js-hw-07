615e6cd29b419540da33f247eaa1eeebb34bd812
Yaroslav Tkach <hrom233@hotmail.com>
2023-04-23 21:43:19 +0300

    Completed HW 2
    1) Completed homework №2;
    2) Update Readme file.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
+});
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(lightbox);
